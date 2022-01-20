95cc14a57dc3d9a72f017934375a3f8090e2b782
Alfred Xiao <alfred.xiao@outlook.com>
2022-01-20 20:13:28 +1100

    added source

---
+++ demo.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "libcustom.h"
+
+int main(){
+    printf("Welcome to my amazing application!\n");
+    vuln_func();
+    return 0;
+}
---
+++ libcustom.h
@@ -0,0 +1,3 @@
+#include <stdio.h>
+
+void vuln_func();
